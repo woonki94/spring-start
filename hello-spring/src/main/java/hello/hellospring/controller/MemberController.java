303a142ee602f26895877cc9ff5a576d5a902920
woonki94 <woonki94@naver.com>
2022-01-03 14:05:24 +0900

    회원등록

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.hellospring.controller;
 
 import hello.hellospring.Service.MemberService;
+import hello.hellospring.domain.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -24,5 +27,20 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService){
         this.memberService = memberService;
+    }
+
+
+    @GetMapping(value = "/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping(value = "/members/new")
+    public String create(MemberForm memberForm){
+        Member member = new Member();
+        member.setName(memberForm.getName());
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }

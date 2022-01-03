4da819a6cf097bbef0d2126800a9f752cf2a007a
woonki94 <woonki94@naver.com>
2022-01-03 13:32:30 +0900

    version1.1

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -11,7 +11,16 @@ public class MemberController {
     /**
     private final MemberService memberService = new MemberService(); // 다른 cotroller도 객체 생성을 할 수 있다.
     **/
+    //필드 주입
+    //@Autowired MemberService memberService
 
+    //setter 주입
+    //private MemberService memberService;
+    //public void setMemberService(MemberService memberService){
+    //    this.memberService = memberService;
+    //}
+
+    //생성자 주입 **권장
     @Autowired
     public MemberController(MemberService memberService){
         this.memberService = memberService;
